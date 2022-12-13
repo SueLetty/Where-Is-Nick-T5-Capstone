1fbb05f90c2b54ca2f91e7af137b8317d1b8f95d
yaping0816 <yaping0816@gmail.com>
2022-12-13 10:26:37 -0800

    finished room class AB#202

--- src/main/java/com.game.whereisnick/model/Room.java
+++ src/main/java/com.game.whereisnick/model/Room.java
@@ -1,5 +1,56 @@
 package com.game.whereisnick.model;
 
-public class Room {
+import java.util.ArrayList;
 
+public class Room {
+  private String name;
+  private String description;
+  private ArrayList<Room> path;
+  private Instructor instructor;
+  private Item key;
+  private Exam exam;
+
+  public Room(String name, String description,
+      ArrayList<Room> path) {
+    this.name = name;
+    this.description = description;
+    this.path = path;
+  }
+
+  public String welcome(){
+    return "Welcome to "+ name;
+  }
+
+  public String conclusionForPassingExam(){
+    return "Congratulations! You passed the exam.";
+  }
+
+  public String conclusionForNotPassingExam(){
+    return "On no. You got it wrong. But don't worry, I'll give you a second chance and try you best this time!";
+  }
+
+
+  public Instructor getInstructor() {
+    return instructor;
+  }
+
+  public void setInstructor(Instructor instructor) {
+    this.instructor = instructor;
+  }
+
+  public Item getKey() {
+    return key;
+  }
+
+  public void setKey(Item key) {
+    this.key = key;
+  }
+
+  public Exam getExam() {
+    return exam;
+  }
+
+  public void setExam(Exam exam) {
+    this.exam = exam;
+  }
 }
