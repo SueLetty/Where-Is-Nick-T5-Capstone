461c213a19458b8615aa304e1a1304ee9357d36c
JpScoot <jamespjones0714@gmail.com>
2022-12-13 14:13:44 -0800

    fixing error after a git pull from remote dev, AB#195

--- src/main/java/com.game.whereisnick/controller/RunGame.java
+++ src/main/java/com.game.whereisnick/controller/RunGame.java
@@ -1,13 +1,19 @@
 package com.game.whereisnick.controller;
 
 import com.game.whereisnick.view.Game;
+import java.util.Scanner;
 
 public class RunGame {
 
   public static void main(String[] args) {
     Game newGame = new Game();
 
+      //create user input for help to get directions
+      Scanner directions = new Scanner(System.in);
+      System.out.println("Feeling loss? type HELP to get further instructions: ");
 
+      String input = directions.nextLine();
+      System.out.println(directions);
 
   }
 
--- src/main/java/com.game.whereisnick/view/Game.java
+++ src/main/java/com.game.whereisnick/view/Game.java
@@ -1,30 +1,52 @@
 package com.game.whereisnick.view;
 
+import java.util.Scanner;
+
 public class Game {
+
 
   public Game() {
     showGameSplash();
     introduction();
+    commandList();
 
   }
 
-  public void showGameSplash(){
-    System.out.println("  __      __.__                           .___          _______  .__        __     ._.\n"
-        + "/ \\    /  \\  |__   ___________   ____   |   | ______  \\      \\ |__| ____ |  | __ | |\n"
-        + "\\   \\/\\/   /  |  \\_/ __ \\_  __ \\_/ __ \\  |   |/  ___/  /   |   \\|  |/ ___\\|  |/ / | |\n"
-        + " \\        /|   Y  \\  ___/|  | \\/\\  ___/  |   |\\___ \\  /    |    \\  \\  \\___|    <   \\|\n"
-        + "  \\__/\\  / |___|  /\\___  >__|    \\___  > |___/____  > \\____|__  /__|\\___  >__|_ \\  __\n"
-        + "       \\/       \\/     \\/            \\/           \\/          \\/        \\/     \\/  \\/"
-        + "\n\n******Press Yes/y OR No/n to continue!************************");
+  public void showGameSplash() {
+    System.out.println(
+        "  __      __.__                           .___          _______  .__        __     ._.\n"
+            + "/ \\    /  \\  |__   ___________   ____   |   | ______  \\      \\ |__| ____ |  | __ | |\n"
+            + "\\   \\/\\/   /  |  \\_/ __ \\_  __ \\_/ __ \\  |   |/  ___/  /   |   \\|  |/ ___\\|  |/ / | |\n"
+            + " \\        /|   Y  \\  ___/|  | \\/\\  ___/  |   |\\___ \\  /    |    \\  \\  \\___|    <   \\|\n"
+            + "  \\__/\\  / |___|  /\\___  >__|    \\___  > |___/____  > \\____|__  /__|\\___  >__|_ \\  __\n"
+            + "       \\/       \\/     \\/            \\/           \\/          \\/        \\/     \\/  \\/"
+            + "\n\n******Press Yes/y OR No/n to continue!************************");
 
 
   }
 
-public void introduction(){
-  System.out.println("Introduction: You are now currently enrolled as a student of TLG Learning facility.\n"
-      + " You will be greeted by Jeanette in the lobby to get started with your orientation.\n"
-      + " Upon completion of orientation, you'll now be navigate through different levels of coding classes in order to graduate\n"
-      + " from the program. Get ready to learn and soak up your mind to become a real software engineer! ");
-}
+  // create introduction method to inform the user of the game
+  public void introduction() {
+    System.out.println(
+        "Introduction: You are now currently enrolled as a student of TLG Learning facility.\n"
+            + " You will be greeted by Jeanette in the lobby to get started with your orientation.\n"
+            + " Upon completion of orientation, you'll now be navigate through different levels of coding classes in order to graduate\n"
+            + " from the program. Get ready to learn and soak up your mind to become a real software engineer! ");
+  }
+
+  // create command list method
+  public void commandList() {
+// create local variables
+    String[] commands = {"North, East, South, West"};
+    String userInput = "";
+    String text = "help";
+
+    if (userInput == text) {
+      System.out.println(commands);
+    }
+
+
+  }
+
 
 }
